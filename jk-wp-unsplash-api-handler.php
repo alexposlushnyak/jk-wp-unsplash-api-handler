e787978c7bbef86ab25554d88de7909594c66822
Alexander Poslushnyak <fr4nk.horr1gan@gmail.com>
2021-02-09 21:49:10 +0200

    Added default getter for api requests

--- jk-wp-unsplash-api-handler.php
+++ jk-wp-unsplash-api-handler.php
@@ -2,13 +2,32 @@
 
 $path = __DIR__;
 
-final class jk_wp_unsplash_init_handler
+class jk_wp_unsplash_init_handler
 {
 
-    public function init()
+    public static function make_request_to_api($params = array(
+        'token' => '',
+        'secret' => '',
+        'page' => '',
+        'per_page' => '',
+        'order_by' => '',
+        'query' => ''
+    ))
     {
 
+        $url = "https://api.unsplash.com/search/photos/?client_id={$params['token']}&page={$params['page']}&per_page={$params['per_page']}&order_by={$params['order_by']}&query={$params['query']}";
 
+        $curl = curl_init($url);
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $resp = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($resp);
 
     }
 
